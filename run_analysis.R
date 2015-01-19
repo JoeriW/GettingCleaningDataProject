0c1cc175e3ace2b726f2b8c6eac26722fac4fe10
JoeriW <willems.joeri85@telenet.be>
2015-01-19 21:33:02 +0100

    second intermediate commitment

--- run_analysis.R
+++ run_analysis.R
@@ -17,8 +17,19 @@ destfile <- "project dataset.zip"
 download.file(fileUrl,destfile = destfile)
 unzip(destfile)
   
-#load the second column of the features.txt file as this column contains the actual features. The first column is a number we don't need.
+#load the second column of the features.txt file as this column contains the actual features.
 featuresVector <- read.table("./UCI HAR DataSet/features.txt")[,2]
+
+#Only keep the the columns containing mean or standard deviations. I decided to not keep the meanFreq() variables.
+featuresVector <- featuresVector[grep(".*-mean[(][)].*|.*std[(][])].*",featuresVector)]
+
+#use more descriptive labels for the featuresVector
+featuresVector <- gsub("^t","time.",featuresVector)
+featuresVector <- gsub("^f","freq.",featuresVector)
+featuresVector <- gsub("-mean[(][])]",".mean",featuresVector)
+featuresVector <- gsub("-std[(][])]",".stdev",featuresVector)
+featuresVector <- gsub("-",".",featuresVector)
+
 
 #load the activity labels
 activityLabels <- read.table("./UCI HAR DataSet/activity_labels.txt")
@@ -34,13 +45,26 @@ trainX <- read.table("./UCI HAR Dataset/train/X_train.txt")
 trainY <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
   
-#the features vector contains the column names of the testX and trainX dataset. Link them together
+#the features vector contains the column names of the testX and trainX dataset. Assign the featuresVector to the column names.
 names(testX) <- featuresVector
 names(trainX)<- featuresVector
 
 #merge the activitylabels with testY and trainY. 
-#give a column name to the merged data
+#give clear names to the merged data colummns
 testY <- merge(testY,activityLabels)
 trainY <- merge(trainY,activityLabels)
 names(testY)<- c("Activity id","Activity")
 names(trainY)<-c("Activity id","Activity")
+
+#give column name to subject column
+names(testSubject)<-"Subject"
+names(trainSubject)<-"Subject"
+
+#merge everything together to create one dataset
+testSet <- cbind(testSubject,testY,testX)
+trainSet <- cbind(trainSubject,trainY,trainX)
+totalSet <- rbind(testSet,trainSet)
+
+
+
+
