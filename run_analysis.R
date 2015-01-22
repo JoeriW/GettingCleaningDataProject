adf2d6d73d7d961d928969885e457faa67e7049d
JoeriW <willems.joeri85@telenet.be>
2015-01-22 21:51:57 +0100

    corrected numbering

--- run_analysis.R
+++ run_analysis.R
@@ -8,12 +8,12 @@
 
 
 
-# 0. install needed packages
+# 1. install needed packages
 
 require(reshape2)
 
 
-# 1. download the zip file from the coursera website and unzip in the workdirectory
+# 2. download the zip file from the coursera website and unzip in the workdirectory
 
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destFile <- "project dataset.zip"
@@ -28,17 +28,17 @@ if(!file.exists(unzippedFile)){
 }
 
 
-# 2. load the second column of the features.txt file as this column contains the actual features.
+# 3. load the second column of the features.txt file as this column contains the actual features.
 
 featuresVector <- read.table("./UCI HAR DataSet/features.txt")[,2]
 
 
-# 3. load the activity labels. 
+# 4. load the activity labels. 
 
 activityLabels <- read.table("./UCI HAR DataSet/activity_labels.txt")
 
 
-# 4. loading test & training datasets (for each set the subject, meausurements and activities are loaded)
+# 5. loading test & training datasets (for each set the subject, meausurements and activities are loaded)
 
 testSubject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 testX <- read.table("./UCI HAR Dataset/test/X_test.txt")
@@ -49,25 +49,25 @@ trainX <- read.table("./UCI HAR Dataset/train/X_train.txt")
 trainY <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
 
-# 5. featuresVector contains the column names for testX and trainX
+# 6. featuresVector contains the column names for testX and trainX
 
 names(testX) = featuresVector
 names(trainX) = featuresVector
 
-# 6. merge the activitylabels with testY and trainY . 
+# 7. merge the activitylabels with testY and trainY . 
 # I only kept the activity name and drop the activity id, but there is no obligation to do so 
 
 testY <- merge(testY,activityLabels)[2]
 trainY <- merge(trainY,activityLabels)[2]
 
 
-# 7. merge everything together to create one dataset
+# 8. merge everything together to create one dataset
 
 testSet <- cbind(testSubject,testY,testX)
 trainSet <- cbind(trainSubject,trainY,trainX)
 totalSet <- rbind(testSet,trainSet)
 
-# 8. Label and give an appropiate name
+# 9. Label and give an appropiate name
 
 names(totalSet)[1] <- "subject"
 names(totalSet)[2] <- "activity"
@@ -77,13 +77,13 @@ totalSet$subject <- factor(totalSet$subject)
 totalSet$activity <- factor(totalSet$activity)
 
 
-# 9. Only keep the the columns containing mean or standard deviations. I decided to not keep the meanFreq() variables 
+# 10. Only keep the the columns containing mean or standard deviations. I decided to not keep the meanFreq() variables 
 # Also keep the activity and subject.id
 
 totalSet <- totalSet[grep(".*subject.*|.*activity.*|.*-mean[(][)].*|.*std[(][])].*",names(totalSet))]
 
 
-# 10. clean up the variables names
+# 11. clean up the variables names
 
 names(totalSet) <- gsub("^t","time.",names(totalSet))
 names(totalSet) <- gsub("^f","freq.",names(totalSet))
@@ -91,12 +91,12 @@ names(totalSet) <- gsub("-mean[(][])]",".mean",names(totalSet))
 names(totalSet) <- gsub("-std[(][])]",".stdev",names(totalSet))
 names(totalSet) <- gsub("-",".",names(totalSet))
 
-# 11. write csv table to working directory (not mandatory)
+# 12. write csv table to working directory (not mandatory)
 
 write.csv(totalSet,"totalset.csv",row.names = FALSE)
 
 
-# 12. create tidy data set with the average of each variable for each activity and subject.
+# 13. create tidy data set with the average of each variable for each activity and subject.
 # First melt the data frame, indicate which columns are the id columns (the first 2 columns, being subject and activity)
 # and which columns are the measure variables (leaving blank means that all non-ids are considered measure variables).
 # Recast the melted set into a dataframe taking the mean of each variable for each subject and activity
@@ -105,7 +105,7 @@ write.csv(totalSet,"totalset.csv",row.names = FALSE)
 meltedSet <- melt(totalSet,id.vars = c(1,2))
 summarySet <- dcast(meltedSet, subject + activity ~ variable,mean)
 
-# 13. Save the final results as txt file. Store into the working directory
+# 14. Save the final results as txt file. Store into the working directory
 
 write.table(summarySet,file = "summaryset.txt",row.names = FALSE)
 
